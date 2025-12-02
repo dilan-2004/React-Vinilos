c88850ded2116b62d753e7c81c598ab0e254de82
dilan-2004 <dilanbarrioscortez@gmail.com>
2025-12-02 16:08:55 -0300

    Add files via upload

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Home from './pages/home';
 import Products from './pages/productos';
 import ProductDetails from './pages/productDetails';
 import Login from './pages/auth/login';
-import CreateUser from './pages/auth/create-user';
+import CreateUser from './pages/auth/Create-User';
 import Blog from './pages/blog';
 import Contacto from './pages/contacto';
 import Nosotros from './pages/nosotros';
